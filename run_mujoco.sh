217bcc1a41a3810cc2dd68f9ac3499f2c7388156
Siwakon <tiger.prasang@gmail.com>
2026-09-12 22:00:43 +0700

    Fix ROS_DISTRO="${ROS_DISTRO:-humble}"

--- run_mujoco.sh
+++ run_mujoco.sh
@@ -8,7 +8,7 @@ cd "$(dirname "$0")"
 ROBOT="${1:?usage: $0 <robot> [launch args]  (robot = stem of src/champ_base_gait_planning/urdf/<robot>.urdf|.xacro)}"
 shift
 
-ROS_DISTRO="${ROS_DISTRO:-jazzy}"
+ROS_DISTRO="${ROS_DISTRO:-humble}"
 source "/opt/ros/${ROS_DISTRO}/setup.bash"
 source install/setup.bash
 ros2 launch champ_base_gait_planning mujoco_sim.launch.py "robot:=${ROBOT}" "$@"
