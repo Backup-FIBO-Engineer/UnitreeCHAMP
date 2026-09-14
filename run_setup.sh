ba0cc6006267ff4e0bc2e56162dc441cdefebf77
Siwakon <tiger.prasang@gmail.com>
2026-09-14 15:16:50 +0700

    Update

---
+++ run_setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+
+# Temporary
+# sudo sysctl -w net.core.rmem_max=2147483647
+# sudo sysctl -w net.core.rmem_default=67108864
+
+# ------
+# Permanent
+# echo "net.core.rmem_max=2147483647" | sudo tee -a /etc/sysctl.conf
+# echo "net.core.rmem_default=67108864" | sudo tee -a /etc/sysctl.conf
+
+
+
+echo "Current kernel UDP receive buffer:"
+sysctl net.core.rmem_max
+sysctl net.core.rmem_default
+echo ""
+
+
+echo "Deactivated conda"
+conda deactivate 2>/dev/null || true
+echo ""
+
+
+
+echo "Setup unitree ros2 environment"
+source /opt/ros/humble/setup.bash
+source $HOME/UnitreeCHAMP/install/setup.bash
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces><NetworkInterface name="enp2s0" priority="default" multicast="default" /></Interfaces><AllowMulticast>spdp</AllowMulticast></General><Internal><SocketReceiveBufferSize min="64MB"/></Internal></Domain></CycloneDDS>'
